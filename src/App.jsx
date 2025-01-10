1d8c038f80068b5c6edf7f6260a9d39db019c44f
Vuong Quyen Mai <59490873+RevoloerO@users.noreply.github.com>
2025-01-10 01:27:23 -0500

    v0.2.1
    Read JSON file and print out

--- src/App.jsx
+++ src/App.jsx
@@ -3,44 +3,60 @@ import { useState } from 'react';
 import './App.css';
 import { useEffect } from 'react';
 import axios from 'axios';
+import genealogyData from './mai-genealogy.json';
 
 //import image
 import headerImg1 from './assets/mai-genealogy-icon.jpeg';
 
-const Information = () =>{
+const Information = ({ familyData }) => {
+  console.log({familyData});
   return (
     <div id='family-tree'>
+      <h1>MAI Family Tree</h1>
+      <div id='family-root'>
+        <div id='family-tree-content-1'>
+          <h2>Family Tree</h2>
+          <ul>
+            {familyData.id}
+          </ul>
+        </div>
+      </div>
 
     </div>
   )
 }
-const Header = () =>{
+const Header = () => {
   const headerTransition = () => {
     document.getElementById("header-img-1").id = 'header-img-2';
     document.getElementById("body-background").classList.add("background");
-  } 
+  }
   return (
     <div id='content'>
       <div className="header">
-        <img id="header-img-1" src={headerImg1} alt="" 
-        onClick={() => headerTransition()} ></img>
+        <img id="header-img-1" src={headerImg1} alt=""
+          onClick={() => headerTransition()} ></img>
       </div>
-      <Information />
-    </div> 
+    </div>
   )
 }
 
 const App = () => {
-  const [data,setData] = useState()
+  const [data, setData] = useState()
+  const [familyData, setFamilyData] = useState([]);
 
-  useEffect(()=>{
-    console.log('effect')
-  })
+  const getFamilyData = () => {
+    setFamilyData(genealogyData);
+  }
+  useEffect(() => {
+    getFamilyData();
+  }, [])
 
   return (
     <div id='body-content'>
       <div id='body-background'></div>
       <Header />
+      {familyData && familyData.length > 0 && data.map((item)=><p>{item.about}</p>)}
+      <Information familyData={familyData} />
     </div>
   )
 }
